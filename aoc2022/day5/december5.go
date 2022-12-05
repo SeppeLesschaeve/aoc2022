ed09b40db4ca14824d93dcac1140b9a8d443f49c
SeppeLesschaeve <lesschaeveseppe@gmail.com>
2022-12-05 22:37:22 +0100

    Major refactorings day 5

--- aoc2022/day5/december5.go
+++ aoc2022/day5/december5.go
@@ -14,14 +14,12 @@ func Day5() {
 	configPart2 := getConfig(configAndSteps[0])
 	steps := getSteps(configAndSteps[1])
 	for _, step := range steps {
-		amount := step[0]
-		for amount > 0 {
-			changeConfigSingle(configPart1, step[1], step[2])
-			amount -= 1
+		for amount := step[0]; amount > 0; amount-- {
+			changeConfig(configPart1, 1, step[1], step[2])
 		}
 	}
 	for _, step := range steps {
-		changeConfigMultiple(configPart2, step[0], step[1], step[2])
+		changeConfig(configPart2, step[0], step[1], step[2])
 	}
 	fmt.Println(getTop(configPart1))
 	fmt.Println(getTop(configPart2))
@@ -29,50 +27,26 @@ func Day5() {
 
 func getTop(config [][]string) string {
 	var top []string
-	i := 0
-	for i < len(config) {
+	for i := 0; i < len(config); i++ {
 		top = append(top, config[i][0])
-		i += 1
 	}
 	return strings.Join(top, "")
-}
-
-func getAmountOfStacks(stacks string) int {
-	amounts := strings.Fields(stacks)
-	return len(amounts)
-}
-
-func firstN(s string, n int) string {
-	if len(s) > n {
-		return s[:n]
-	}
-	return s
-}
-
-func trimLeftChars(s string, n int) string {
-	m := 0
-	for i := range s {
-		if m >= n {
-			return s[i:]
-		}
-		m++
-	}
-	return s[:0]
 }
 
 func getLine(line string) []string {
 	var stacks []string
 	for len(line) > 0 {
-		stack := firstN(line, 4)
-		if stack == "    " {
-			stacks = append(stacks, "")
+		if len(line) >= 4 {
+			stack := line[:4]
+			if stack == "    " {
+				stacks = append(stacks, "")
+			} else {
+				stacks = append(stacks, string(stack[1]))
+			}
+			line = line[4:]
 		} else {
-			stacks = append(stacks, string(stack[1]))
-		}
-		if len(line) == 3 {
-			line = ""
-		} else {
-			line = trimLeftChars(line, 4)
+			stacks = append(stacks, string(line[1]))
+			break
 		}
 	}
 	return stacks
@@ -80,64 +54,34 @@ func getLine(line string) []string {
 
 func getConfig(configString string) [][]string {
 	lines := strings.Split(configString, "\n")
-	amountOfStacks := getAmountOfStacks(lines[len(lines)-1])
-	config := make([][]string, amountOfStacks)
-	i := 0
-	for i <= len(lines)-2 {
+	config := make([][]string, len(strings.Fields(lines[len(lines)-1])))
+	for i := 0; i <= len(lines)-2; i++ {
 		line := getLine(lines[i])
-		l := 0
-		for l < len(line) {
-			crate := line[l]
+		for l, crate := range line {
 			if crate != "" {
 				config[l] = append(config[l], crate)
 			}
-			l += 1
 		}
-		i += 1
 	}
 	return config
-}
-
-func getStep(stepString string) []int {
-	var amount, from, to int
-	_, _ = fmt.Sscanf(stepString, "move %d from %d to %d", &amount, &from, &to)
-	return []int{amount, from - 1, to - 1}
 }
 
 func getSteps(stepsString string) [][]int {
 	lines := strings.Split(stepsString, "\n")
 	steps := make([][]int, len(lines))
-	i := 0
-	for i < len(lines) {
-		step := getStep(lines[i])
-		steps[i] = step
-		i += 1
+	for i, line := range lines {
+		var amount, from, to int
+		_, _ = fmt.Sscanf(line, "move %d from %d to %d", &amount, &from, &to)
+		steps[i] = []int{amount, from - 1, to - 1}
 	}
 	return steps
 }
 
-func pop(stack []string, amount int) ([]string, []string) {
-	if len(stack) < amount {
-		panic("stack is empty")
-	}
-	var popped []string
-	amountToPop := 0
-	for amountToPop < amount {
-		popped = append(popped, stack[amountToPop])
-		amountToPop += 1
-	}
-	stack = stack[amount:]
-	return popped, stack
-}
-
-func changeConfigSingle(config [][]string, from int, to int) {
+func changeConfig(config [][]string, amount int, from int, to int) {
 	var headFrom []string
-	headFrom, config[from] = pop(config[from], 1)
-	config[to] = append(headFrom, config[to]...)
-}
-
-func changeConfigMultiple(config [][]string, amount int, from int, to int) {
-	var headFrom []string
-	headFrom, config[from] = pop(config[from], amount)
+	for amountToPop := 0; amountToPop < amount; amountToPop++ {
+		headFrom = append(headFrom, config[from][amountToPop])
+	}
+	config[from] = config[from][amount:]
 	config[to] = append(headFrom, config[to]...)
 }

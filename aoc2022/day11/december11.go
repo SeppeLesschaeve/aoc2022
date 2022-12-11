c21cedffdcd88412db28e821004cd49f1ac087c8
SeppeLesschaeve <lesschaeveseppe@gmail.com>
2022-12-11 11:43:06 +0100

    Refactorings day 11

--- aoc2022/day11/december11.go
+++ aoc2022/day11/december11.go
@@ -8,14 +8,9 @@ import (
 	"strings"
 )
 
-type Operation struct {
-	amount string
-	op     string
-}
-
 type Monkey struct {
 	items          []int
-	operation      Operation
+	operation      func(int, int, *Monkey)
 	div            int
 	throwsTo       []int
 	amountOfThrows int
@@ -27,35 +22,19 @@ func Day11() {
 	monkeysSplit := strings.Split(day11Content, "\n\n")
 	monkeysPart1 := getMonkeys(monkeysSplit)
 	monkeysPart2 := getMonkeys(monkeysSplit)
-	monkeysPart1 = simulateRounds(monkeysPart1, 20, true)
-	monkeysPart2 = simulateRounds(monkeysPart2, 10000, false)
-	monkeyBusinessPart1 := getMonkeyBusiness(monkeysPart1)
-	monkeyBusinessPart2 := getMonkeyBusiness(monkeysPart2)
-	fmt.Println(monkeyBusinessPart1)
-	fmt.Println(monkeyBusinessPart2)
+	monkeysPart1 = simulateRounds(monkeysPart1, 20, func(i int, monkey *Monkey) { monkey.items[i] /= 3 })
+	biggest := getBiggest(monkeysPart2)
+	monkeysPart2 = simulateRounds(monkeysPart2, 10000, func(i int, monkey *Monkey) { monkey.items[i] %= biggest })
+	fmt.Println(getMonkeyBusiness(monkeysPart1))
+	fmt.Println(getMonkeyBusiness(monkeysPart2))
 }
 
-func simulateRounds(monkeys []*Monkey, rounds int, trust bool) []*Monkey {
-	biggest := getBiggest(monkeys)
+func simulateRounds(monkeys []*Monkey, rounds int, trust func(int, *Monkey)) []*Monkey {
 	for round := 0; round < rounds; round++ {
 		for _, monkey := range monkeys {
 			for itemInd, item := range monkey.items {
-				var amount int
-				if monkey.operation.amount == "old" {
-					amount = item
-				} else {
-					amount, _ = strconv.Atoi(monkey.operation.amount)
-				}
-				if monkey.operation.op == "*" {
-					monkey.items[itemInd] = item * amount
-				} else if monkey.operation.op == "+" {
-					monkey.items[itemInd] = item + amount
-				}
-				if trust {
-					monkey.items[itemInd] /= 3
-				} else {
-					monkey.items[itemInd] %= biggest
-				}
+				monkey.operation(item, itemInd, monkey)
+				trust(itemInd, monkey)
 				if monkey.items[itemInd]%monkey.div == 0 {
 					monkeys[monkey.throwsTo[0]].items = append(monkeys[monkey.throwsTo[0]].items, monkey.items[itemInd])
 				} else {
@@ -81,48 +60,42 @@ func getMonkeys(monkeysSplit []string) []*Monkey {
 	var monkeys []*Monkey
 	for _, monkeySplit := range monkeysSplit {
 		monkeyInfo := strings.Split(monkeySplit, "\n")
-		items := getItems(monkeyInfo[1])
-		operation := getOperation(monkeyInfo[2])
-		div := getDiv(monkeyInfo[3])
-		throwsTo := getThrowsTo(monkeyInfo[4], monkeyInfo[5])
-		monkeys = append(monkeys, &Monkey{items, operation, div, throwsTo, 0})
+		var items []int
+		var itemsInfo, op, a string
+		var div, trueMonkey, falseMonkey int
+		monkeyInfo[1] = strings.ReplaceAll(monkeyInfo[1], ", ", ",")
+		_, _ = fmt.Sscanf(monkeyInfo[1], "  Starting items: %s", &itemsInfo)
+		_, _ = fmt.Sscanf(monkeyInfo[2], "  Operation: new = old %s %s", &op, &a)
+		_, _ = fmt.Sscanf(monkeyInfo[3], "  Test: divisible by %d", &div)
+		_, _ = fmt.Sscanf(monkeyInfo[4], "    If true: throw to monkey %d", &trueMonkey)
+		_, _ = fmt.Sscanf(monkeyInfo[5], "    If false: throw to monkey %d", &falseMonkey)
+		itemsSplit := strings.Split(itemsInfo, ",")
+		for _, item := range itemsSplit {
+			itemInt, _ := strconv.Atoi(item)
+			items = append(items, itemInt)
+		}
+		if op == "*" {
+			monkeys = append(monkeys, &Monkey{items, func(item int, itemInd int, monkey *Monkey) {
+				monkey.items[itemInd] = item * getAmount(a, item)
+			}, div, []int{trueMonkey, falseMonkey}, 0})
+		} else if op == "+" {
+			monkeys = append(monkeys, &Monkey{items, func(item int, itemInd int, monkey *Monkey) {
+				monkey.items[itemInd] = item + getAmount(a, item)
+			}, div, []int{trueMonkey, falseMonkey}, 0})
+		}
 	}
 	return monkeys
 }
 
-func getItems(itemsInfo string) []int {
-	var items []int
-	itemsInfo = itemsInfo[18:]
-	itemsSplit := strings.Split(itemsInfo, ", ")
-	for _, item := range itemsSplit {
-		itemInt, _ := strconv.Atoi(item)
-		items = append(items, itemInt)
+func getAmount(a string, item int) int {
+	if a != "old" {
+		amount, _ := strconv.Atoi(a)
+		return amount
 	}
-	return items
-}
-
-func getOperation(itemsInfo string) Operation {
-	var op, amount string
-	_, _ = fmt.Sscanf(itemsInfo[13:], "new = old %s %s", &op, &amount)
-	return Operation{amount, op}
-}
-
-func getDiv(itemsInfo string) int {
-	var div int
-	_, _ = fmt.Sscanf(itemsInfo, "  Test: divisible by %d", &div)
-	return div
-}
-
-func getThrowsTo(trueThrow string, falseThrow string) []int {
-	var trueMonkey, falseMonkey int
-	_, _ = fmt.Sscanf(trueThrow, "    If true: throw to monkey %d", &trueMonkey)
-	_, _ = fmt.Sscanf(falseThrow, "    If false: throw to monkey %d", &falseMonkey)
-	return []int{trueMonkey, falseMonkey}
+	return item
 }
 
 func getMonkeyBusiness(monkeys []*Monkey) int {
-	sort.Slice(monkeys, func(p, q int) bool {
-		return monkeys[p].amountOfThrows > monkeys[q].amountOfThrows
-	})
+	sort.Slice(monkeys, func(p, q int) bool { return monkeys[p].amountOfThrows > monkeys[q].amountOfThrows })
 	return monkeys[0].amountOfThrows * monkeys[1].amountOfThrows
 }
